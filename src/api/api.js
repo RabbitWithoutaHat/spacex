e61590143720748235ab89c5e8a5c220dc061463
aleksandra volobueva <Aleksandra.Volobueva@glidewelldental.com>
2021-01-22 17:35:58 +0300

    add loader

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,11 @@
 import Axios from "axios"
+import { loadProgressBar } from "axios-progress-bar"
+import "axios-progress-bar/dist/nprogress.css"
 
 const baseURL = "https://api.spacexdata.com/v4"
 const axios = Axios.create({ baseURL })
+
+loadProgressBar("", axios)
 
 export const api = {
   getAllLaunches: async () => axios.get(`/launches`),
